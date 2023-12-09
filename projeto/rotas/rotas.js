4ac31cef6fdd5fe8d1614d57752fe88497bb0102
Eugenio <joaovitor.eugenio15@gmail.com>
2023-12-09 00:18:22 -0300

    feat: adicionado arquivo de rotas

---
+++ projeto/rotas/rotas.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+const { verificaToken } = require("../middlewares/token/token.js");
+
+const controllerServidor = require("../controllers/servidor")
+const controllerUsuario = require("../controllers/usuario");
+const controllerAuth = require("../controllers/auth");
+const controllerEquipe = require("../controllers/equipe");
+
+// Rota padrão de verificação
+router.get("/", controllerServidor.getHealth);
+router.get("/versao", controllerServidor.getVersao);
+
+//Rotas de autenticação
+router.post("/auth/registrar", controllerAuth.registrar);
+router.post("/auth/login", controllerAuth.login);
+router.get("/auth/verificar-token", controllerAuth.verificarToken);
+
+// Rotas de usuário
+router.get("/usuario/:usuario", verificaToken, controllerUsuario.getUsuario);
+
+//rotas equipes
+router.post("/equipe/registrar", controllerEquipe.registrar)
+router.get("/equipe/listar", verificaToken, controllerEquipe.listar)
+
+module.exports = router;
