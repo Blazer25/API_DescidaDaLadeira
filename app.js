b4e9530c383f2b0c34a972f33f3ef61b4665af45
Eugenio <joaovitor.eugenio15@gmail.com>
2023-12-09 00:12:48 -0300

    feat: Desenvolvido app do projeto.

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const rotas = require("./projeto/rotas/rotas.js");
+const conexaoMongo = require("./projeto/servicos/mongo/conexaoMongo.js");
+
+const PORTA = 3000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/", rotas);
+
+conexaoMongo().then(() => {
+  app.listen(PORTA);
+  console.log(`Servidor online na porta ${PORTA}`);
+});
