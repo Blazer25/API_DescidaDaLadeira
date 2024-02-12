de94ee8321d40278589d2141b8e29adf168e3878
João Eugênio <joaovitor.eugenio15@gmail.com>
2024-02-12 16:05:18 -0300

    feat: adicionadas rotas de alterar equipe e registrar corrida.

--- projeto/rotas/rotas.js
+++ projeto/rotas/rotas.js
@@ -7,6 +7,7 @@ const controllerServidor = require("../controllers/servidor")
 const controllerUsuario = require("../controllers/usuario");
 const controllerAuth = require("../controllers/auth");
 const controllerEquipe = require("../controllers/equipe");
+const controllerCorrida = require("../controllers/corrida");
 
 // Rota padrão de verificação
 router.get("/", controllerServidor.getHealth);
@@ -18,10 +19,15 @@ router.post("/auth/login", controllerAuth.login);
 router.get("/auth/verificar-token", controllerAuth.verificarToken);
 
 // Rotas de usuário
-router.get("/usuario/:usuario", verificaToken, controllerUsuario.listaUsuario);
+router.get("/usuario/:usuario", verificaToken, controllerUsuario.listar);
 
 //rotas equipes
 router.post("/equipe/registrar", controllerEquipe.registrar)
+router.patch("/equipe/alterar/:codigoEquipe", verificaToken, controllerEquipe.alterar)
 router.get("/equipe/listar", verificaToken, controllerEquipe.listar)
+
+//rotas corridas
+router.post("/corrida/registrar", verificaToken, controllerCorrida.registrar)
+
 
 module.exports = router;
