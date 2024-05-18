97030840671d720063d4f2ff583b0c6c8b664575
João Eugênio <joaovitor.eugenio15@gmail.com>
2024-05-17 21:17:07 -0300

    feat: adicionando porta ao .env

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const rotas = require("./projeto/rotas/rotas.js");
 const conexaoMongo = require("./projeto/servicos/mongo/conexaoMongo.js");
 
-const PORTA = 3000;
+const PORTA = process.env.PORTA ? Number(process.env.PORTA) : 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
